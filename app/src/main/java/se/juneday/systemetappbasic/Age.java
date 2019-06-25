b063f1c5322522615dc14f3dc08a9b982bec3acd
kuojinNerverland <gusjhakjca@student.gu.se>
2019-06-25 19:15:16 +0200

    Fler ändringar gjordes för att möjliggöra kompileringen.

--- app/src/main/java/se/juneday/systemetappbasic/Age.java
+++ app/src/main/java/se/juneday/systemetappbasic/Age.java
@@ -3,6 +3,7 @@ package se.juneday.systemetappbasic;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 public class Age extends AppCompatActivity {
@@ -15,11 +16,13 @@ public class Age extends AppCompatActivity {
         setContentView(R.layout.activity_age);
 
         b = findViewById(R.id.button);
-
         b.setOnClickListener(view -> {
             Intent i = new Intent(Age.this, MainActivity.class);
             startActivity(i);
         });
 
+    }
+
+    public void onClick(View view) {
     }
 }
