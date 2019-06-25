a630d7977d10f06c4093073a48e2f1c755f8d902
kuojinNerverland <gusjhakjca@student.gu.se>
2019-06-25 18:51:34 +0200

    Fler ändringar gjordes för att möjliggöra kompileringen.

--- app/src/main/java/se/juneday/systemetappbasic/Age.java
+++ app/src/main/java/se/juneday/systemetappbasic/Age.java
@@ -1,11 +1,9 @@
 package se.juneday.systemetappbasic;
 
-            import android.content.Intent;
-            import android.support.v7.app.AppCompatActivity;
-            import android.os.Bundle;
-            import android.view.View;
-            import android.widget.Button;
-            import android.widget.CheckBox;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class Age extends AppCompatActivity {
 
@@ -17,6 +15,7 @@ public class Age extends AppCompatActivity {
         setContentView(R.layout.activity_age);
 
         b = findViewById(R.id.button);
+
         b.setOnClickListener(view -> {
             Intent i = new Intent(Age.this, MainActivity.class);
             startActivity(i);
